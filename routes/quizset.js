9bda47d29ab272900010c37ada18bb24cd56fa18
kanisshka <76627599+kanisshka@users.noreply.github.com>
2023-05-25 14:35:51 +0530

    yo

--- routes/quizset.js
+++ routes/quizset.js
@@ -4,7 +4,7 @@ import QuizsetController from "../controllers/Quizsetcontroller.js";
 
 router.get('/',QuizsetController.getAllquiz)
 router.post('/',QuizsetController.createquiz)
-// router.get('/:id',QuizsetController.getoneOrder)
+router.get('/:id',QuizsetController.getSingleDocById)
 router.get('/find/:user',QuizsetController.getAllquizbyUser)
 // router.delete('/:id',QuizsetController.dltDocById)
 
